74c004d4b86c3b7b98719fa78e60d62b2baa46e7
xiaotanyu13 <xiaotanyu13@163.com>
2018-03-27 20:11:33 +0800

    2018-03-27 18:13:52更新spring-boot版本，修改Condition方式

--- spring-cloud-starter-thrift-client/src/main/java/com/icekredit/rpc/thrift/client/properties/ThriftClientPropertiesCondition.java
+++ spring-cloud-starter-thrift-client/src/main/java/com/icekredit/rpc/thrift/client/properties/ThriftClientPropertiesCondition.java
@@ -1,30 +1,24 @@
 package com.icekredit.rpc.thrift.client.properties;
 
-import org.apache.commons.collections4.MapUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.autoconfigure.condition.ConditionOutcome;
 import org.springframework.boot.autoconfigure.condition.SpringBootCondition;
-import org.springframework.boot.bind.RelaxedPropertyResolver;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 
-import java.util.Map;
 
 public class ThriftClientPropertiesCondition extends SpringBootCondition {
 
-    private static final String SPRING_THRIFT_CLIENT = "spring.thrift.client.";
-    private static final String SPRING_THRIFT_CLIENT_PACKAGE_TO_SCAN = "package-to-scan";
-    private static final String SPRING_THRIFT_CLIENT_SERVICE_MODEL = "service-model";
+    private static final String SPRING_THRIFT_CLIENT_PACKAGE_TO_SCAN = "spring.thrift.client.package-to-scan";
+    private static final String SPRING_THRIFT_CLIENT_SERVICE_MODEL = "spring.thrift.client.service-model";
 
     @Override
     public ConditionOutcome getMatchOutcome(ConditionContext context, AnnotatedTypeMetadata metadata) {
-        RelaxedPropertyResolver resolver = new RelaxedPropertyResolver(context.getEnvironment());
-        Map<String, Object> properties = resolver.getSubProperties(SPRING_THRIFT_CLIENT);
+        String packageToScan = context.getEnvironment().getProperty(SPRING_THRIFT_CLIENT_PACKAGE_TO_SCAN);
+        String serviceModel = context.getEnvironment().getProperty(SPRING_THRIFT_CLIENT_SERVICE_MODEL);
 
-        return new ConditionOutcome(MapUtils.isNotEmpty(properties)
-                && properties.containsKey(SPRING_THRIFT_CLIENT_PACKAGE_TO_SCAN)
-                && StringUtils.isNotBlank(MapUtils.getString(properties, SPRING_THRIFT_CLIENT_PACKAGE_TO_SCAN)
-        ), "Thrift server service model is " + MapUtils.getString(properties, SPRING_THRIFT_CLIENT_SERVICE_MODEL));
+        return new ConditionOutcome(StringUtils.isNotBlank(packageToScan)
+                , "Thrift server service model is " + serviceModel);
 
     }
 
